bf313250d6a4dbb7b360481538fa8fcb28d18413
Patryk <patryk.wal@onet.pl>
2021-06-12 01:08:56 +0200

    new idea

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WAE_Projekt
 
-zrodlo : 
+zrodlo :
 https://github.com/LDNN97/Evolutionary-Optimization-Algorithms
 
 inne zrodla:
@@ -8,3 +8,5 @@ https://github.com/CyberAgent/cmaes/blob/main/cmaes/_cma.py
 https://github.com/ammackenzie/Webots-Universal-Controller-and-Evolutionary-Robotics-Suite/blob/master/CMAES.py
 https://github.com/srom/cma-es/blob/master/cma/core.py
 https://github.com/LDNN97/Evolutionary-Optimization-Algorithms/blob/master/opti/cmaes.py
+
+https://www.mdpi.com/1999-4893/12/3/56/htm
--- additional_methods/clearing.py
+++ additional_methods/clearing.py
@@ -1,27 +1,30 @@
 import random
-import itertools
+
 
 def d(a, b):
     return random.choices(range(abs(a[0] - b[0])))[0]
 
+
 def fitness(a):
     return a[1]
 
-def clearing(population, sigma, kappa):
+
+def clearing(population, sigma=1, kappa=2):
     s = sorted(population, key=fitness)
     s.reverse()
     n = len(s)
     for i in range(n):
-        if fitness(s[i])>0 :
+        if fitness(s[i]) > 0:
             nbWinner = 1
             for j in range(i+1, n):
-                if fitness(s[i])>0 and d(s[i], s[j]) < sigma:
+                if fitness(s[i]) > 0 and d(s[i], s[j]) < sigma:
                     if nbWinner < kappa:
                         nbWinner += 1
-                    else : 
-                        #fitness(s[j]) = 0
-                        s[j] = (s[j][0], 0)      
+                    else:
+                        # fitness(s[j]) = 0
+                        s[j] = (s[j][0], 0)
     print(s)
+
 
 if __name__ == "__main__":
     tmp = list()
