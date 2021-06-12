59afe089b52a96c6f8713e2e0133d3c204df6562
Patryk <patryk.wal@onet.pl>
2021-06-12 13:03:55 +0200

    clearing update

--- additional_methods/clearing.py
+++ additional_methods/clearing.py
@@ -1,34 +1,36 @@
-import random
+import math
 
 
 def d(a, b):
-    return random.choices(range(abs(a[0] - b[0])))[0]
+    len_a = len(a)
+    assert len_a == len(b), "Dimensions must be equal."
+    sum = 0
+    for i in range(len_a):
+        sum += (a[i] - b[i]) ** 2
+    return math.sqrt(sum)
 
 
-def fitness(a):
-    return a[1]
-
-
-def clearing(population, sigma=1, kappa=2):
-    s = sorted(population, key=fitness)
+def clearing(population, sigma=2, kappa=2):
+    s = sorted(population, key=lambda s: s[1])
     s.reverse()
     n = len(s)
     for i in range(n):
-        if fitness(s[i]) > 0:
+        if s[i][1] > 0:
             nbWinner = 1
             for j in range(i+1, n):
-                if fitness(s[i]) > 0 and d(s[i], s[j]) < sigma:
+                if s[i][1] > 0 and d(s[i][0], s[j][0]) < sigma:
                     if nbWinner < kappa:
                         nbWinner += 1
                     else:
-                        # fitness(s[j]) = 0
                         s[j] = (s[j][0], 0)
-    print(s)
+    s.sort(key=lambda s: s[1])
+    return s
 
 
 if __name__ == "__main__":
     tmp = list()
     for i in range(9):
         # number, fitness
-        tmp.append((i, abs(4 - i)))
-    clearing(tmp, 2.5, 4)
+        tmp.append(([i], abs(4 - i)))
+    res = clearing(tmp, 2.5, 4)
+    print(res)
